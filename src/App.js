caf0ebb3731ffd2ee4b018a5dc1fe6ca19c7ce42
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-11-29 19:23:22 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import '@stripe/stripe-js '
 import AOS from "aos";
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
 import "aos/dist/aos.css";
 
 function App() {
@@ -12,10 +12,31 @@ function App() {
       once: true,
     });
   }, []);
-  return (
-    <>
-    </>
-  )
+
+  const getOnLineStatus = () => navigator.onLine;
+
+  const useNavigatorOnLine = () => {
+    const [status, setStatus] = useState(getOnLineStatus());
+
+    const setOnline = () => setStatus(true);
+    const setOffline = () => setStatus(false);
+
+    useEffect(() => {
+      window.addEventListener("online", setOnline);
+      window.addEventListener("offline", setOffline);
+
+      return () => {
+        window.removeEventListener("online", setOnline);
+        window.removeEventListener("offline", setOffline);
+      };
+    }, []);
+    console.log(status);
+    return status;
+  };
+
+  useNavigatorOnLine();
+
+  return <></>;
 }
 
-export default App
+export default App;
