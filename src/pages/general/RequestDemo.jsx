94bc7472265e748cde879e5f40ecf85e4e7d4d4e
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-11-30 11:05:21 +0100

    added request demo

--- src/pages/general/RequestDemo.jsx
+++ src/pages/general/RequestDemo.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 export default function RequestDemo() {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = "https://widgets.tucalendi.com/assets/iframewidget_cf.js";
+    script.async = true;
+
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
   return (
-    <div>RequestDemo</div>
-  )
+    <div className="pt-5">
+      <div
+        id="tucalendi_iframe_root"
+        data-tucalendi-domain="mopheth.tucalendi.com/mentorfy/mentorfy-demo"
+        data-tucalendi-options=""
+      ></div>
+    </div>
+  );
 }
