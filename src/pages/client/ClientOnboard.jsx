138f83ef2e349a8c6cc1383944df0a9c2b8c3943
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-12-01 03:50:22 +0100

    fix

--- src/pages/client/ClientOnboard.jsx
+++ src/pages/client/ClientOnboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
 import { Password } from "primereact/password";
 import { useFormik } from "formik";
 import { useRecoilState } from "recoil";
@@ -13,7 +14,7 @@ export default function ClientOnboard() {
   const [registration, setRegistration] = useRecoilState(registerUserAtom);
   const [provinces, setProvinces] = useState([]);
   const countries = ["Canada", "Others"];
-
+  const stages = 7
   const onSubmit = async (values) => {
     const { user, ...others } = registration;
     const payload = {
@@ -24,7 +25,7 @@ export default function ClientOnboard() {
       },
     };
   };
-// stage 1
+  // stage 1
   const proceed = () => {
     const payload = {
       ...registration,
@@ -37,7 +38,7 @@ export default function ClientOnboard() {
     };
     setRegistration(payload);
   };
-// stage 2
+  // stage 2
   const saveCountry = () => {
     const payload = {
       ...registration,
@@ -49,7 +50,7 @@ export default function ClientOnboard() {
     };
     setRegistration(payload);
   };
-// stage 3
+  // stage 3
   const saveProvices = () => {
     const payload = {
       ...registration,
@@ -63,22 +64,22 @@ export default function ClientOnboard() {
     };
     setRegistration(payload);
   };
-// stage 4
-const proceedToWorkspace = () => {
-  const payload = {
-    ...registration,
-    onboardStep: 5,
+  // stage 4
+  const proceedToWorkspace = () => {
+    const payload = {
+      ...registration,
+      onboardStep: 5,
+    };
+    setRegistration(payload);
   };
-  setRegistration(payload);
-};
 
-const previous = (data)=> {
-  const payload = {
-    ...registration,
-    onboardStep: data,
+  const previous = (data) => {
+    const payload = {
+      ...registration,
+      onboardStep: data,
+    };
+    setRegistration(payload);
   };
-  setRegistration(payload);
-}
 
   const {
     values,
@@ -183,8 +184,11 @@ const previous = (data)=> {
             ) : registration.onboardStep === 2 ? (
               <div className="main py-5">
                 <div className="flex items-center justify-between w-full pb-2">
-                  <i className="pi pi-arrow-left" onClick={()=> previous(1)}></i>
-                  <div className="">{registration.onboardStep}/6</div>
+                  <i
+                    className="pi pi-arrow-left"
+                    onClick={() => previous(1)}
+                  ></i>
+                  <div className="">{registration.onboardStep}/{stages}</div>
                 </div>
                 <div className="header font-['ginto-bold'] text-2xl text-center pb-5">
                   Select a country
@@ -219,8 +223,11 @@ const previous = (data)=> {
             ) : registration.onboardStep === 3 ? (
               <div className="main py-5">
                 <div className="flex items-center justify-between w-full pb-2">
-                <i className="pi pi-arrow-left" onClick={()=> previous(2)}></i>
-                  <div className="">{registration.onboardStep}/6</div>
+                  <i
+                    className="pi pi-arrow-left"
+                    onClick={() => previous(2)}
+                  ></i>
+                  <div className="">{registration.onboardStep}/{stages}</div>
                 </div>
                 <div className="header font-['ginto-bold'] text-2xl text-center pb-5">
                   Province & other details
@@ -289,25 +296,164 @@ const previous = (data)=> {
             ) : registration.onboardStep === 4 ? (
               <div className="main py-5">
                 <div className="flex items-center justify-between w-full pb-2">
-                  <div className="ml-auto">{registration.onboardStep}/6</div>
+                  <div className="ml-auto">{registration.onboardStep}/{stages}</div>
                 </div>
                 <div className=" text-center text-5xl">🎉</div>
                 <div className="header font-['ginto-bold'] font-black text-2xl text-center pb-5">
-                  Welcome, <br/> {registration.user.fullName}
+                  Welcome, <br /> {registration.user.fullName}
                 </div>
 
                 <div className="grid gap-3">
-                  <p className="pb-10 text-center">Let’s get your workspace configured</p>
+                  <p className="pb-10 text-center">
+                    Let’s get your workspace configured
+                  </p>
+                  <button className="pri-btn" onClick={proceedToWorkspace}>
+                    Next
+                  </button>
+                </div>
+              </div>
+            ) : registration.onboardStep === 5 ? (
+              <div className="main py-5">
+                <div className="flex items-center justify-between w-full pb-2">
+                  <i
+                    className="pi pi-arrow-left"
+                    onClick={() => previous(4)}
+                  ></i>
+                  <div className="">{registration.onboardStep}/{stages}</div>
+                </div>
+                <div className="header font-['ginto-bold'] text-2xl text-center pb-5">
+                  🙌 Right on! configure your Workspace
+                </div>
+
+                <div className="grid gap-3">
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="username">Workspace Name</label>
+                    <InputText
+                      id="username"
+                      name="workspaceName"
+                      aria-describedby="username-help"
+                      value={values.workspaceName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.workspaceName && touched.workspaceName && (
+                      <p className="error">{errors.workspaceName}</p>
+                    )}
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="username">Areas of Interest</label>
+                    <InputText
+                      id="username"
+                      name="professionalArea"
+                      aria-describedby="username-help"
+                      value={values.professionalArea}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.professionalArea && touched.professionalArea && (
+                      <p className="error">{errors.professionalArea}</p>
+                    )}
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="username">Workspace Description</label>
+                    <InputTextarea
+                      id="username"
+                      name="description"
+                      aria-describedby="username-help"
+                      cols={4}
+                      rows={4}
+                      value={values.description}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.description && touched.description && (
+                      <p className="error">{errors.description}</p>
+                    )}
+                  </div>
                   <button
                     className="pri-btn"
-                    onClick={proceedToWorkspace}
+                    disabled={errors.country}
+                    onClick={saveCountry}
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
+            ) : registration.onboardStep === 6 ? (
+              <div className="main py-5">
+                <div className="flex items-center justify-between w-full pb-2">
+                  <i
+                    className="pi pi-arrow-left"
+                    onClick={() => previous(5)}
+                  ></i>
+                  <div className="">{registration.onboardStep}/{stages}</div>
+                </div>
+                <div className="header font-['ginto-bold'] text-2xl text-center pb-5">
+                  Province & other details
+                </div>
+
+                <div className="grid gap-3">
+                  <div className="grid md:grid-cols-2 gap-2 w-full h-full">
+                    <div className="flex flex-col gap-2">
+                      <label htmlFor="username">Province</label>
+                      <Dropdown
+                        id="username"
+                        name="province"
+                        value={values.province}
+                        options={provinces}
+                        optionLabel="Province"
+                        optionValue="Province"
+                        className=" !text-black"
+                        filter
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      {errors.province && touched.province && (
+                        <p className="error">{errors.province}</p>
+                      )}
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <label htmlFor="username">Phone Number</label>
+                      <InputText
+                        id="phone"
+                        name="phone"
+                        aria-describedby="phone-help"
+                        value={values.phone}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      {errors.phone && touched.phone && (
+                        <p className="error">{errors.phone}</p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="username">Postal code</label>
+                    <InputText
+                      id="postalcode"
+                      name="postalcode"
+                      aria-describedby="postalcode-help"
+                      value={values.postalcode}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.postalcode && touched.postalcode && (
+                      <p className="error">{errors.postalcode}</p>
+                    )}
+                  </div>
+                  <button
+                    className="pri-btn"
+                    disabled={
+                      errors.phone || errors.postalcode || errors.province
+                    }
+                    onClick={saveProvices}
                   >
                     Next
                   </button>
                 </div>
               </div>
             ) : (
-              ""
+              registration.onboardStep === 7("")
             )}
           </div>
         </div>
