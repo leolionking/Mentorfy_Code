5f2e02f869015ed1cb6d36cbb032b13b375e261c
donemmx <emmanuelidus@gmail.com>
2023-12-07 10:17:45 +0100

    fix

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Sidebar({ links }) {
-  const myLinks = [
-    { name: "Dashboard", link: "/dashboard", icon: "home" },
-    { name: "Mentors", link: "/dashboard", icon: "user" },
-    { name: "Mentees", link: "/dashboard", icon: "sitemap" },
-    { name: "Workspace Insights", link: "/dashboard", icon: "chart-bar" },
-    { name: "Plans & Billing", link: "/dashboard", icon: "credit-card" },
-    { name: "Settings", link: "/dashboard", icon: "cog" },
-  ];
 
   return (
     <div className="hidden lg:block">
@@ -17,7 +9,7 @@ export default function Sidebar({ links }) {
         <div className="sidebar p-5 h-full">
           <div className="logo">{/* <img src={logo} alt="logo" /> */}</div>
           <div className="flex flex-col gap-4 mt-20">
-            {myLinks.map((link, i) => (
+            {links.map((link, i) => (
                 <div className="myLink flex items-center gap-2">
                     <i className={"p-3 pi pi-"+ link.icon}></i>
                     <Link to={link.link} className="link" key={i}>{link.name}</Link>
