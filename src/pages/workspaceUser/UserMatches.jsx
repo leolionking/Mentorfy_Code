ac6a61fbd6dbb35dcce79e708f2d3732ee9e0733
donemmx <emmanuelidus@gmail.com>
2023-12-28 19:49:38 +0100

    fix

--- src/pages/workspaceUser/UserMatches.jsx
+++ src/pages/workspaceUser/UserMatches.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserCard from "../../components/UserCard";
+import { workspaceStore } from "../../atom/workspaceAtom";
+import { useRecoilValue } from "recoil";
+import { authState } from "../../atom/authAtom";
+import { getUserByWorkspace } from "../../utils/general/generalApi";
+import { getMatchingByMentee } from "../../utils/mentee/menteeApi";
+import { getMatchingByMentor } from "../../utils/mentor/mentorApi";
 
 export default function UserMatches() {
+  const auth = useRecoilValue(authState);
+  const workspace = useRecoilValue(workspaceStore);
+  const [matches, setMatches] = useState([]);
+
+  const getMatches = () => {
+    const payload = {
+      sessionID: auth?.sessionID,
+      userId: auth?.username,
+      status: "new",
+      workspaceId: workspace?.id,
+    };
+
+    getUserByWorkspace(payload).then((res) => {
+      const newPayload = {
+        sessionID: auth?.sessionID,
+        id: res.payload[0].id,
+        status: "new",
+      };
+
+      if (auth.role === "mentor") {
+        getMatchingByMentor(newPayload).then((res) => {
+          setMatches(res.payload);
+        });
+      }
+      {
+        getMatchingByMentee(newPayload).then((res) => {
+          setMatches(res.payload);
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    getMatches();
+  }, []);
   return (
     <div>
       <h1 className="text-xl lg:text-2xl font-['ginto-bold']">
@@ -12,8 +53,8 @@ export default function UserMatches() {
       </p>
       <div className="py-10">
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((res) => (
-            <UserCard  />
+          {matches.map((res) => (
+            <UserCard />
           ))}
         </div>
       </div>
