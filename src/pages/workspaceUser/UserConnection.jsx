1a662605da46ec7ebc7d72ef5a22ad8436ab427c
donemmx <emmanuelidus@gmail.com>
2023-12-28 20:21:52 +0100

    fix

--- src/pages/workspaceUser/UserConnection.jsx
+++ src/pages/workspaceUser/UserConnection.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserCard from '../../components/UserCard';
 
 export default function UserConnection() {
+  const [connections, setConnections] = useState([]);
+
   return (
-    <div>MentorConnection</div>
+    <div>
+    <h1 className="text-xl lg:text-2xl font-['ginto-bold']">
+      My Connections
+    </h1>
+    <p className="text-sm">
+      Temporary Text you have no these mentees suggestions
+    </p>
+    <div className="py-10">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5">
+        {connections.map((res) => (
+          <UserCard />
+        ))}
+      </div>
+    </div>
+  </div>
   )
 }
