c5c24f126082def6730044cb5811e8f4c769fb7c
donemmx <emmanuelidus@gmail.com>
2023-12-28 18:06:03 +0100

    fix

--- src/components/dashboard/UserDashboardHeader.jsx
+++ src/components/dashboard/UserDashboardHeader.jsx
@@ -1,5 +1,5 @@
 import { Avatar } from "primereact/avatar";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { workspaceStore } from "../../atom/workspaceAtom";
 import { useRecoilValue } from "recoil";
@@ -7,23 +7,24 @@ import { user } from "../../atom/userAtom";
 import { authState } from "../../atom/authAtom";
 
 export default function UserDashboardHeader() {
-    const workspace = useRecoilValue(workspaceStore);
-    const userData = useRecoilValue(user);
-    const auth = useRecoilValue(authState);
-    return (
+  const [open, setOpen] = useState(true);
+  const workspace = useRecoilValue(workspaceStore);
+  const userData = useRecoilValue(user);
+  const auth = useRecoilValue(authState);
+  return (
+    <>
       <div className="h-[70px] bg-white shadow-small w-full lg:w-[calc(100vw-17vw)] fixed top-0 right-0 ml-auto z-[500] p-4 px-10">
         <div className="w-full">
           <div className="flex items-center justify-between">
             <div className="logo flex items-center gap-2">
-              <div className="h-[40px] w-[40px]">
-              </div>
+              <div className="h-[40px] w-[40px]"></div>
             </div>
             <div className=" hidden lg:flex items-center gap-4">
-              <Link to={"/" +auth.role + "-notifications"}>
+              <Link to={"/" + auth.role + "-notifications"}>
                 <i className="pi pi-bell"></i>
               </Link>
               <Link to="/select-workspace" className="text-sm">
-               Switch Workspace
+                Switch Workspace
               </Link>
               <div className="flex items-center gap-2">
                 <Avatar
@@ -36,5 +37,20 @@ export default function UserDashboardHeader() {
           </div>
         </div>
       </div>
-    );
-  }
+      {open ? (
+        <div className=" fixed rounded-lg bg-white shadow-small z-50 w-[150px] text-sm top-[70px] right-10 ">
+          <div className="flex items-center gap-4 pb-3 bg-white cursor-pointer px-7 p-3 rounded-t-lg  hover:bg-gray-200">
+            <i className="pi pi-user text-sm"></i>
+            Profile
+          </div>
+          <div className="flex bg-white items-center gap-4 cursor-pointer hover:bg-gray-200 px-7 p-3 rounded-b-lg">
+            <i className="pi pi-power-off text-sm"></i>
+            Logout
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
